b4ba6ea2017df5c4986deba0721772ea62550e3f
workingjubilee <workingjubilee@gmail.com>
2019-03-27 14:56:52 -0700

    Add migrations and router skeleton

---
+++ routers/cohortRouter.js
@@ -0,0 +1,10 @@
+// #### cohorts
+
+// - `id`: primary key, auto-increments.
+// - `name`: text, required.
+
+// #### students
+
+// - `id`: primary key, auto-increments.
+// - `name`: text, required.
+// - `cohort_id`: references the `id` in the cohorts table.
